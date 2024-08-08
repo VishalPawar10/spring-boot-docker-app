4cc99180148bd544467356717a96d5dca16fb1d7
Vishal <vishalsureshpawar@gmail.com>
2024-08-08 09:55:00 +0530

    m1 method commited

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -37,6 +37,9 @@ public class WelcomeRestController {
 		logger.info("***** ***************************** *****");
 		return msg;
 	}
+	public void m1(){
+		//m1 method created
+	}
 	
 	public void m2(){
 		//method m2() created
